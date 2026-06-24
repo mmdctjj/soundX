a182cc9a9eb08ef88b7f6fe185d5b812176ad9cd
mmdctjj <984808285@mail.com>
2026-06-24 18:06:29 +0800

    feat: 修改图标问题

--- apps/mobile/src/components/XiaoAiIcon.tsx
+++ apps/mobile/src/components/XiaoAiIcon.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ImageStyle, StyleProp } from "react-native";
 import { Image as ExpoImage } from "expo-image";
+import { useTheme } from "../context/ThemeContext";
 
 interface XiaoAiIconProps {
   size?: number;
@@ -13,9 +14,15 @@ export const XiaoAiIcon: React.FC<XiaoAiIconProps> = ({
   color,
   style,
 }) => {
+  const { theme } = useTheme();
+  const source =
+    theme === "dark"
+      ? require("../../assets/images/xiaoai_dark.png")
+      : require("../../assets/images/xiaoai_light.png");
+
   return (
     <ExpoImage
-      source={require("../../assets/dexopt/xiaoai.svg")}
+      source={source}
       style={[{ width: size, height: size }, style]}
       tintColor={color}
       contentFit="contain"
